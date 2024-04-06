f9eec99c4153ea0a2e878a9515d0c7747d0484f8
als244 <andrew.sheinberg@gmail.com>
2024-04-05 20:34:21 -0400

    fixing copying 0 bytes

--- job_stats_test.c
+++ job_stats_test.c
@@ -14,11 +14,11 @@ void insert_job_to_db(sqlite3 * jobs_db, Job * job){
 										job -> job_id,
 										job -> user,
 										job -> group,
-										&(job -> n_nodes),
-										&(job -> n_cpus),
-										&(job -> n_gpus),
-										&(job -> mem_mb),
-										&(job -> billing),
+										job -> n_nodes,
+										job -> n_cpus,
+										job -> n_gpus,
+										job -> mem_mb,
+										job -> billing,
 										job -> time_limit,
 										job -> submit_time,
 										job -> node_list,
@@ -29,7 +29,7 @@ void insert_job_to_db(sqlite3 * jobs_db, Job * job){
 										job -> exit_code);
 
 	
-	printf("Would have inserted\n\n:%s\n", full_insert_statement);
+	printf("Would have inserted:\n%s\n", full_insert_statement);
 }
 
 
@@ -40,29 +40,72 @@ int parse_req_tres(Job * job){
 
 	char * req_tres = job -> req_tres;
 	int len = strlen(req_tres);
+	printf("Len = %d\n", len);
+
 	int comma_cnt = 0;
 	for (int i = 0; i < len; i++){
 		if (req_tres[i] == ','){
+			printf("Saw comma at i = %d\n", i);
 			comma_cnt += 1;
 		}
 	}
+	int billing = 0;
+	int n_nodes = 0;
+	int n_cpus = 0;
+	int n_gpus = 0;
+	int mem_mb = 0;
 	if (comma_cnt == 3){
-		sscanf(req_tres, "billing=%d,cpu=%d,mem=%dM,node=%d", &(job -> billing), &(job -> n_cpus), &(job -> mem_mb), &(job -> n_nodes));
-		job -> n_gpus = 0;
+		sscanf(req_tres, "billing=%d,cpu=%d,mem=%dM,node=%d", &billing, &n_cpus, &mem_mb, &n_nodes);
 	}
-	else if (comma_cnt == 4){
-		sscanf(req_tres, "billing=%d,cpu=%d,gres/gpu=%d,mem=%dM,node=%d", &(job -> billing), &(job -> n_cpus), &(job -> n_gpus), &(job -> mem_mb), &(job -> n_nodes));
-	}
-	else{
-		return -1;
+	if (comma_cnt == 4){
+		sscanf(req_tres, "billing=%d,cpu=%d,gres/gpu=%d,mem=%dM,node=%d", &billing, &n_cpus, &n_gpus, &mem_mb, &n_nodes);
 	}
 
+	job -> billing = billing;
+	job -> n_nodes = n_nodes;
+	job -> n_cpus = n_cpus;
+	job -> n_gpus = n_gpus;
+	job -> mem_mb = mem_mb;
+	
 	return 0;
 
 }
 
 
 
+void scan_line(char *line, Job * job)
+{
+    char * tok = strtok(line, "|");
+    if (strlen(tok) > 0){
+    	strcpy(job -> user, tok);
+    }
+    tok = strtok(NULL, "|");
+    strcpy(job -> group, tok);
+    
+    tok = strtok(NULL, "|");
+    job -> job_id = atol(tok);
+
+    tok = strtok(NULL, "|");
+    strcpy(job -> req_tres, tok);
+    parse_req_tres(job);
+
+    tok = strtok(NULL, "|");
+    strcpy(job -> time_limit, tok);
+    tok = strtok(NULL, "|");
+    strcpy(job -> submit_time, tok);
+    tok = strtok(NULL, "|");
+    strcpy(job -> node_list, tok);
+    tok = strtok(NULL, "|");
+    strcpy(job -> start_time, tok);
+    tok = strtok(NULL, "|");
+    strcpy(job -> end_time, tok);
+    tok = strtok(NULL, "|");
+    strcpy(job -> elapsed_time, tok);
+    tok = strtok(NULL, "|");
+    strcpy(job -> state, tok);
+    tok = strtok(NULL, "|");
+    strcpy(job -> exit_code, tok);
+}
 
 void dump_sacct_file(sqlite3 * jobs_db, char * outfile){
 
@@ -89,7 +132,7 @@ void dump_sacct_file(sqlite3 * jobs_db, char * outfile){
 
 
 	while(fgets(buffer, sizeof(buffer), sacct_file)){
-		printf("Buffer: %s\n", buffer);
+		/*
 		ret_cnt = sscanf(buffer, "%s[^|]|%s[^|]|%ld|%s[^|]|%s[^|]|%s[^|]|%s[^|]|%s[^|]|%s[^|]|%s[^|]||%s[^|]|%s",
 			job -> user,
 			job -> group,
@@ -103,22 +146,15 @@ void dump_sacct_file(sqlite3 * jobs_db, char * outfile){
 			job -> elapsed_time,
 			job -> state,
 			job -> exit_code);
+		*/
+		scan_line(buffer, job);
 		
-		
-		printf("User: %s\nGroup: %s\nJob ID:%ld\n", job -> user, job -> group, job -> job_id);
 		// error reading line
 		if (job -> user == NULL){
 			continue;
 		}
 
-		err = parse_req_tres(job);
-
-		if (err == -1){
-			continue;
-		}
-
 		insert_job_to_db(NULL, job);
-
 
 	}
 
@@ -133,7 +169,7 @@ void dump_sacct_file(sqlite3 * jobs_db, char * outfile){
 void collect_job_stats(sqlite3 * jobs_db, char * out_dir, char * hostname, long time_sec){
 
 	char * cmd;
-	asprintf(&cmd, "sacct --nodelist=%s --format=User,Group,JobID,ReqTRES,Timelimit,Submit,NodeList,Start,End,Elapsed,State,ExitCode --state=COMPLETED,CANCELLED,FAILED,TIMEOUT,OUT_OF_MEMORY --starttime=now-1day --endtime=now --unit=M --allusers -P", hostname);
+	asprintf(&cmd, "sacct --nodelist=%s --format=User,Group,JobID,ReqTRES,Timelimit,Submit,NodeList,Start,End,Elapsed,State,ExitCode --state=COMPLETED,CANCELLED,FAILED,TIMEOUT,OUT_OF_MEMORY --starttime=now-1day --endtime=now --unit=M --allusers -P -n", hostname);
 
 	char * outfile;
 	asprintf(&outfile, "%s/%s_temp.txt", out_dir, hostname);
